631c3f1b4cb3d014ed725b97ec8496de60093409
kenju <priv.kenju@gmail.com>
2012-07-10 03:10:46 +0900

    None

--- tools/graph.py
+++ tools/graph.py
@@ -1,31 +1,31 @@
 #! /usr/bin/python
 
 import os;
-import re;
-RE_ENTRY = re.compile( "(.+)\s(.+)\s\((.+)!(.+)\+(.*)\)" );
+import sys;
 
 def read_stack( f ):
-	recs = [ i.strip().decode( "utf-8" )
+	recs = [ i.strip().split()[-1].decode( "utf-8" )
 			 for i in open( f, "rb" ).readlines() if len( i.strip() ) >0 ];
 	retval = [];
-	for i in reversed( recs ):
+	for i in recs:
 		parts = i.split();
-		( dll, func ) = parts[2][1:-1].split( "!" );
-		func = func.split( "+" )[0].replace( "0x", "_" );
-		retval.append( dll + "_" + func );
+		l = parts[-1].replace( "0x", "" );
+		if l.startswith( "0x" ):
+			l = l.replace( "0x", "" );
+		elif l.startswith( "(" ):
+			l = l[1:-1].split( "+", 2 )[0];
+		
+		retval.append( '"{0}"'.format( l ) );
 
-	retval.append( "ReadFile" );
-	
 	return retval;
 
 stacks = [];
-dups = [];
 
 def main( d ):
 	for i in os.listdir( d ):
 		if i.endswith( "txt" ) == False:
 			continue;
-		
+		i = os.path.join( d, i );
 		s = read_stack( i );
 		prev = s[0];
 		for addr in s[1:]:
@@ -35,8 +35,8 @@ def main( d ):
 			prev = addr;
 
 	print( "digraph callstack {" );
-	for c in stacks:
+	for c in reversed( stacks ):
 		print( "{0} -> {1};".format( *c ) );
 	print( "}" );
 
-main( "./" );
+main( sys.argv[1] );
