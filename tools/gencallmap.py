98bf2b3927ae1d1a82ed27e57a436a862ce8abc0
kenju <priv.kenju@gmail.com>
2012-07-19 04:15:15 +0900

    None

--- tools/gencallmap.py
+++ tools/gencallmap.py
@@ -7,69 +7,86 @@ import json;
 OUTPUT = {};
 
 def split_line( line ):
-	line = line.translate( None, "\n\r" );
-	line = line.replace( "\t", " " );
-	line = re.sub( "\..*text:", "", line );
-	return [ i for i in line.split( " " ) if len( i ) > 0 ];
+	line = line.translate( None, b"\n\r" );
+	line = line.replace( b"\t", b" " );
+	line = re.sub( b"\..*text:", b"", line );
+	return [ i for i in line.split( b" " ) if len( i ) > 0 ];
 
 def get_module_name( fd ):
 	while True:
-		line = fd.readline();
+		line = fd.readline().strip();
 		if len( line ) == 0:
 			return None;
-		if line.find( "File Name" ) == -1:
+		if line.find( b"File Name" ) == -1:
 			continue;
 		items = split_line( line );
-		return items[-1].split( "\\" )[-1];
+		return items[-1].split( b"\\" )[-1].decode();
 
 def get_imagebase( fd ):
 	while True:
-		line = fd.readline();
+		line = fd.readline().strip();
 		if len( line ) == 0:
 			return -1
-		if line.find( "Imagebase" ) == -1:
+		if line.find( b"Imagebase" ) == -1:
 			continue;
 		items = split_line( line );
 		return int( items[-1], 16 );
 
-def get_callret_pairs( fd, libcall_only ):
-	callret_pairs = [];
+def get_procs_and_callrets( fd, libcall_only ):
+	procs = [];
+	callrets = [];
+	
 	while True:
-		line = fd.readline();
+		line = fd.readline().strip();
 		if len( line ) == 0:
 			break;
-		if line.find( "call" ) == -1:
-			continue;
 		
-		items = split_line( line );
-		if len( items ) < 3:
-			sys.stderr.write( "Unkonwn call:%s\n" % line );
-			continue;
-		if items[1] != "call":
-			continue;
-		if items[2].find( "ds:" ) == -1 and libcall_only == True:
-			continue;
-		call_addr = int( items[0], 16 );
-		call_asm = " ".join( items[1:] );
-		
-		line = fd.readline();
-		if len( line ) == 0:
-			break;
-		items = split_line( line );
-		ret_addr = int( items[0], 16 );
-		ret_asm = " ".join( items[1:] );
-		
-		callret_pairs.append( ( call_addr, call_asm, ret_addr ) );
-	return callret_pairs;
+		# find procedure
+		if line.endswith( b"proc near" ):
+			items = split_line( line );
+			proc_addr = int( items[0], 16 );
+			proc_name = items[1];
+			procs.append( proc_name.decode() );
+			
+		# find call and ret point
+		elif line.find( b"call" ) != -1:
+			items = split_line( line );
 
+			# validations
+			if len( items ) < 3:
+				sys.stderr.write( b"Unkonwn call:%s\n" % line );
+				continue;
+			if items[1] != b"call":
+				continue;
+			if items[2].find( b"ds:" ) == -1 and libcall_only == True:
+				continue;
+
+			# call point
+			call_addr = int( items[0], 16 );
+			call_asm = b" ".join( items[1:] );
+			
+			# ret porint
+			line = fd.readline().strip();
+			if len( line ) == 0:
+				break;
+			items = split_line( line );
+			ret_addr = int( items[0], 16 );
+			#ret_asm = b" ".join( items[1:] );
+			
+			callrets.append( ( call_addr,
+							   call_asm.decode(),
+							   ret_addr ) );
+	
+	return ( procs, callrets );
 
 def error( msg ):
 	sys.stderr.write( msg + "\n" );
 	sys.exit( -1 );
 
-KEY_MOD_NAME = "module_name";
+KEY_MOD_NAME  = "module_name";
 KEY_IMAGEBASE = "imagebase";
-KEY_CALLRETS = "callrets";
+KEY_PROCS     = "procs";
+KEY_CALLRETS  = "callrets";
 
 if __name__ == "__main__":
 	if len( sys.argv ) < 2:
@@ -88,12 +105,11 @@ if __name__ == "__main__":
 		if imagebase == -1:
 			error( "Imagebase not found" );
 
-		callret_pairs = get_callret_pairs( fd, is_libcall_only );
-		if len( callret_pairs ) == 0:
-			error( "Callret pairs parse failed" );
-
-		output[ KEY_MOD_NAME ] = mod_name;
-		output[ KEY_IMAGEBASE ] = imagebase
-		output[ KEY_CALLRETS ] = callret_pairs;
+		( procs, callrets ) = get_procs_and_callrets( fd, is_libcall_only );
+		
+		output[ KEY_MOD_NAME ]  = mod_name;
+		output[ KEY_IMAGEBASE ] = imagebase;
+		output[ KEY_PROCS ]     = procs;
+		output[ KEY_CALLRETS ]  = callrets;
 		
 	print( json.dumps( output ) );
