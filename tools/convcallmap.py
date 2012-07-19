a5b884c5a3c73cdf99f5ff2d5cafec9db04c7589
kenju <priv.kenju@gmail.com>
2012-07-20 04:40:44 +0900

    None

--- tools/convcallmap.py
+++ tools/convcallmap.py
@@ -3,6 +3,7 @@
 
 import sys;
 import sqlite3;
+import json;
 
 SCHEMA = """
 create table modules (
@@ -25,13 +26,19 @@ create table callrets (
 );
 """;
 
-def conv_to_sqlite3( db_file, callmap ):
-	with sqlite3.connect( db_file ):
-		conn.executemany( SCHEMA );
+def init_db( db_file ):
+	try:
+		with sqlite3.connect( db_file ) as conn:
+			for stmt in SCHEMA.split( ";" ):
+				conn.execute( stmt );
+	except:
+		pass;
 
+def conv_to_sqlite3( db_file, callmap ):
+	with sqlite3.connect( db_file ) as conn:
 		# insert to modules table
 		conn.execute(
-			"insert into modules values( ?, ? )",
+			"insert into modules ( name, imagebase ) values( ?, ? )",
 			( callmap[ "module_name" ]
 			  ,callmap[ "imagebase" ] )
 			);
@@ -39,16 +46,21 @@ def conv_to_sqlite3( db_file, callmap ):
 		# insert to procs table
 		for ( addr, name )  in callmap[ "procs" ]:
 			conn.execute(
-				"insert into procs values( ?, ? )",
+				"insert into procs ( address, name ) values( ?, ? )",
 				( addr,
 				  name ) );
-
+		
 		# insert to callrets table
 		for ( call_addr, call_dst, ret_addr ) in callmap[ "callrets" ]:
 			conn.execute(
-				"insert into callrets values( ?, ?, ? )",
+				"insert into callrets ( call, ret, call_dst ) values( ?, ?, ? )",
 				( call_addr,
 				  ret_addr,
 				  call_dst ) );
 
-		
+if __name__ == "__main__":
+	callmap_file = sys.argv[1];
+	db_file = sys.argv[2];
+	init_db( db_file );
+	with open( callmap_file, "rb" ) as fd:
+		conv_to_sqlite3( db_file, json.load( fd ) );
