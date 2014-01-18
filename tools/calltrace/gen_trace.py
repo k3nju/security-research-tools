ab184bec614aa425f89d2cf29be885280b6ecc64
kenju <priv.kenju@gmail.com>
2014-01-18 11:26:59 +0900

    none

--- tools/calltrace/gen_trace.py
+++ tools/calltrace/gen_trace.py
@@ -11,7 +11,7 @@ import lstparse;
 
 SUPPRESS_DLLS = [
 	# system
-	"ntd.dll",
+	"ntdll.dll",
 	"user32.dll",
 	"comdlg32.dll",
 	"comctl32.dll",
@@ -19,6 +19,11 @@ SUPPRESS_DLLS = [
 	"msvcrt.dll",
 	"msvcr90.dll",
 	"windowscodecs.dll",
+	"shell32.dll",
+	"kernelbase.dll",
+	"kernel32.dll",
+	"ole32.dll",
+	"sysfer.dll",
 
 	# others
 	"libsvn_tsvn32.dll",
@@ -66,8 +71,8 @@ class CallStackSet:
 			return None
 		return self.__modules[name]
 
-	def get_callstacks(self):
-		for callstack in self.__callstacks:
+	def get_callstacks(self, max_count):
+		for callstack in self.__callstacks[:max_count]:
 			# return "return addresses"
 			yield (i for i in callstack)
 		
@@ -84,9 +89,9 @@ def load_lstfiles(lstfile_dir):
 	
 	return lstfiles
 
-def print_text(callstack_set, lstfile_map):
+def print_text(callstack_set, lstfile_map, cs_max):
 	# render call stack for each
-	for callstack in callstack_set.get_callstacks():
+	for callstack in callstack_set.get_callstacks(cs_max):
 		# traverse callstack
 		print("==========================================");
 		for mod_name, _unused, ret_addr in callstack:
@@ -103,29 +108,45 @@ def print_text(callstack_set, lstfile_map):
 				call_addr, func_name = e
 				print("{0} 0x{1:08X} {2}".format(mod_name, call_addr, func_name.decode()));
 
-def print_dot(callstack_set, lstfile_map):
+def suppress_callstack(callstack):
+	suppressed = [];
+	prev = None;
+	registered = False
+	
+	for elem in callstack:
+		mod_name, call_addr, ret_addr = elem;
+		if mod_name in SUPPRESS_DLLS and prev == mod_name:
+			if registered == False:
+				suppressed.append((mod_name, 0, 0));
+				registered = True
+			continue;
+		registered = False
+		prev = mod_name;
+		suppressed.append((mod_name, call_addr, ret_addr));
+
+	return suppressed;
+
+
+def print_dot(callstack_set, lstfile_map, cs_max):
 	def str_node(s):
 		return re.sub("\+|\.|\:|\[|\]|(\s+)", "_", s);
 	
 	call_set = set([]);
-	for callstack in callstack_set.get_callstacks():
-		callstack = list(callstack)
+	color_set = set([]);
+
+	def set_color(mod_name, node_name):
+		if mod_name in SUPPRESS_DLLS:
+			color_set.add("{0} [fontcolor = black]".format(node_name));
+		else:
+			color_set.add("{0} [fontcolor = red]".format(node_name));
+	
+	for callstack in callstack_set.get_callstacks(cs_max):
+		callstack = suppress_callstack(list(callstack));
+		#callstack = list(callstack);
 		callstack.reverse()
-		caller_dll = ""
-		prev_suppressed = None;
 		caller = "root"
 		for mod_name, _unused, ret_addr in callstack:
-			if mod_name == caller_dll and mod_name in SUPPRESS_DLLS:
-				suppressed = "SUPPRESSED_{0}".format(str_node(mod_name));
-				call_set.add("{0} -> SUPRESSED_{1}".format(str_node(caller), suppressed));
-				prev_suppressed = suppressed;
-				continue;
-
-				
-			
-			if prev_suppressed != None:
-				callee = prev_suppressed;
-			elif mod_name not in lstfile_map:
+			if mod_name not in lstfile_map:
 				callee = "Unknown_{0}_ret_{1:08X}".format( mod_name, ret_addr)
 			else:
 				lstfile = lstfile_map[mod_name]
@@ -138,18 +159,19 @@ def print_dot(callstack_set, lstfile_map):
 					mod_name,
 					call_addr,
 					func_name.decode());
-			
-			call_set.add("{0} -> {1}".format(str_node(caller), str_node(callee)))
+
+			set_color(mod_name, str_node(callee));
+			call_set.add("{0} -> {1}".format(str_node(caller), str_node(callee)));
 			caller = callee
-			caller_dll = mod_name;
-			
 
 	print("digraph callstack {")
+	for color in color_set:
+		print(color)
 	for call in call_set:
 		print(call)
 	print("}")
 
-def main(callstack_json_file, lstfiles_dir, dot):
+def main(callstack_json_file, lstfiles_dir, dot, cs_max):
 	callstack_set = CallStackSet(callstack_json_file)
 	lstfiles = load_lstfiles(lstfiles_dir)
 
@@ -167,9 +189,9 @@ def main(callstack_json_file, lstfiles_dir, dot):
 		lstfile_map[name] = lstfile;
 
 	if dot == None:
-		print_text(callstack_set, lstfile_map);
+		print_text(callstack_set, lstfile_map, cs_max);
 	else:
-		print_dot(callstack_set, lstfile_map);
+		print_dot(callstack_set, lstfile_map, cs_max);
 
 if __name__ == "__main__":
 	# usage: python gen_trace.py --cs "path to callstack.json" --lstdir "path to directory of lstfiles"
@@ -178,10 +200,10 @@ if __name__ == "__main__":
 	parser.add_argument("--lstdir", dest = "lstdir", required = True); # path to directory of lstfiles
 	#parser.add_argument("--all_cs", dest = "all_cs", default = False, action = "store_true");
 	parser.add_argument("--dot", dest = "dot", default = None, action = "store_true" ); # print as dot file format
+	parser.add_argument("--cs_max", dest="cs_max", default = "300");
 
 	args = parser.parse_args();
-
-	main(args.cs, args.lstdir, args.dot);
+	main(args.cs, args.lstdir, args.dot, int(args.cs_max))
 	
 	
 	
